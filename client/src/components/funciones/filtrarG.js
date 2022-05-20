6611033a14067bec31ac80fac67c34caa85f834c
alemosmusi <alemosmusi@gmail.com>
2022-05-20 11:47:52 -0300

    arreglado filtro

--- client/src/components/funciones/filtrarG.js
+++ client/src/components/funciones/filtrarG.js
@@ -1,20 +1,66 @@
-export default function(a,search){
-    var encontrado = []
-for(var i = 0; i<a.length ; i++){
-//   console.log(a[i].genres)
-  for(var j = 0; j<a[i].genres.length; j++){
-    // console.log(a[i].genres[j].name)
-    for(var z = 0; z<search.length; z++){
-      if(a[i].genres[j].name.toLowerCase() === search[z]){
-        encontrado.push(a[i])
+const filtrarG = function(a,search){
+  //     var encontrado = []
+  // for(var i = 0; i<a.length ; i++){
+  // //   console.log(a[i].genres)
+  //   for(var j = 0; j<a[i].genres.length; j++){
+  //     // console.log(a[i].genres[j].name)
+  //     for(var z = 0; z<search.length; z++){
+  //       if(a[i].genres[j].name.toLowerCase() === search[z]){
+  //         encontrado.push(a[i])
+          
+          
+  //       }
         
+  //     }
+  //   }
+  // }
+  // let hash = {};
+  // encontrado = encontrado.filter(o => hash[o.name] ? false : hash[o.name] = true);
+  // return encontrado
+  
+  
+  var listf = a
+  
+    if(search.length === 1){
+      var listf2 = []
+      for (let i = 0; i < listf.length; i++) {
+        for (let j = 0; j < listf[i].genres.length; j++) {
+          // console.log(listf[i].genres[j])
+         if(listf[i].genres[j].name.toLowerCase() === search[0]){
+           listf2.push(listf[i])
+
+           console.log(listf2)
+         }    
+        
+        }
+      
+      }
+      return listf2
+     }
+    if(search.length > 1){
+      var listf2 = []
+      for (let i = 0; i < listf.length; i++) {
+       for (let j = 0; j < listf[i].genres.length; j++) {
+         if(listf[i].genres[j].name.toLowerCase() === search[0]){
+           listf2.push(listf[i])
+         }    
+        
+        }
         
       }
-      
+      var gens = search.slice(1)
+      return filtrarG(listf2, gens)
     }
+    return listf
+  
+  
+  
+  
+  
+  
+  
+  
+  
   }
-}
-let hash = {};
-encontrado = encontrado.filter(o => hash[o.name] ? false : hash[o.name] = true);
-return encontrado
-}
+
+export default filtrarG
